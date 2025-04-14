891533d72bfd84b9f38b4ba2049e4c3d35d67f6f
Nithish211220 <nithish.y@jmangroup.com>
2025-04-14 22:11:02 +0530

    wfh

---
+++ models/intermediate/int_invoice_fx_converted.sql
@@ -0,0 +1,34 @@
+
+with joined as (
+  select
+    inv.invoice_id,
+    inv.invoicedate,
+    date_trunc('month', cast(inv.invoicedate as date)) as billing_month,
+    inv.user_id,
+    usr.city,
+    usr.country,
+    inv.productname,
+    inv.invoiceamount,
+
+    fx.reportingtransfxrate,
+    fx.reportinglocalfxrate,
+    fx.budgettransfxrate,
+    fx.budgetlocalfxrate
+
+  from {{ ref('stg_invoices') }} inv
+  left join {{ ref('stg_users') }} usr on inv.user_id = usr.user_id
+  left join {{ ref('stg_invoice_reporting') }} fx on inv.invoice_id = fx.invoice_id
+)
+
+select
+  *,
+  -- Reporting INR Conversion
+  invoiceamount * (reportingtransfxrate / reportinglocalfxrate) as reporting_local,
+  -- Reporting EUR
+  (invoiceamount * (reportingtransfxrate / reportinglocalfxrate)) * budgetlocalfxrate as reporting_eur,
+
+  -- Budget INR
+  invoiceamount * (budgettransfxrate / budgetlocalfxrate) as budget_local,
+  -- Budget EUR
+  (invoiceamount * (budgettransfxrate / budgetlocalfxrate)) * budgetlocalfxrate as budget_eur
+from joined
---
+++ models/marts/fct_monthly_billings.sql
@@ -0,0 +1,13 @@
+select
+  billing_month,
+  productname,
+  country,
+  city,
+
+  sum(reporting_local) as total_reporting_local,
+  sum(reporting_eur) as total_reporting_eur,
+  sum(budget_local) as total_budget_local,
+  sum(budget_eur) as total_budget_eur
+
+from {{ ref('int_invoice_fx_converted') }}
+group by 1, 2, 3, 4
